11dd6f642ae49f689c92bebcca2ff4e43a0e8d22
Giacomo Accursi <giacomo.accursi@gmail.com>
2021-05-17 16:10:42 +0200

    added project

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
