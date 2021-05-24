d1d0f0ad2bd97179ebbae83e4a00adaf171eaf03
Giacomo Accursi <giacomo.accursi@gmail.com>
2021-05-24 13:07:04 +0200

    messo a posto homeActivity

--- AndroidApp/app/src/main/java/com/example/next2me/HomeActivity.java
+++ AndroidApp/app/src/main/java/com/example/next2me/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,41 +23,34 @@ import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
-    User user;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        getUserInfo();
         updateUI();
     }
 
-    private void getUserInfo(){
+    public void updateUI(){
         String uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
-        FirebaseDatabase.getInstance().getReference(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference userTable = DatabaseHelper.getInstance().getDb().getReference("Users");
+        userTable.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                user = dataSnapshot.getValue(User.class);
+                User user = dataSnapshot.getValue(User.class);
+                //DatabaseHelper dbh = DatabaseHelper.getInstance();
+                // ((ImageView)findViewById(R.id.profilePic)).setImageBitmap(dbh.getPhotoFromStorage(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()));
+                ((TextView)findViewById(R.id.name)).setText(user.getName());
+                ((TextView)findViewById(R.id.surname)).setText(user.getSurname());
+                ((TextView)findViewById(R.id.birthdate)).setText(user.getBirthdate());
+                ((TextView)findViewById(R.id.gender)).setText(user.getGender());
+                ((TextView)findViewById(R.id.preferences)).setText(user.getPreferences());
+                ((TextView)findViewById(R.id.mail)).setText(user.getEmail());
+                ((TextView)findViewById(R.id.description)).setText(user.getDescription());
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.e("db","Error while reading data");
             }
         });
     }
-
-    private void updateUI() {
-        DatabaseHelper dbh = DatabaseHelper.getInstance();
-        ((ImageView)findViewById(R.id.profilePic)).setImageBitmap(dbh.getPhotoFromStorage(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()));
-        ((TextView)findViewById(R.id.name)).setText(user.getName());
-        ((TextView)findViewById(R.id.surname)).setText(user.getSurname());
-        ((TextView)findViewById(R.id.birthdate)).setText(user.getBirthdate());
-        ((TextView)findViewById(R.id.gender)).setText(user.getGender());
-        ((TextView)findViewById(R.id.preferences)).setText(user.getPreferences());
-        ((TextView)findViewById(R.id.mail)).setText(user.getEmail());
-        ((TextView)findViewById(R.id.description)).setText(user.getDescription());
-    }
-
 }
--- AndroidApp/app/src/main/java/com/example/next2me/utils/DatabaseHelper.java
+++ AndroidApp/app/src/main/java/com/example/next2me/utils/DatabaseHelper.java
@@ -9,13 +9,17 @@ import com.example.next2me.data.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Objects;
 
 public class DatabaseHelper {
 
@@ -77,6 +81,8 @@ public class DatabaseHelper {
         DatabaseReference userTable = db.getReference("Users");
         userTable.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user);
     }
+
+
 
     public FirebaseDatabase getDb() {
         return db;
