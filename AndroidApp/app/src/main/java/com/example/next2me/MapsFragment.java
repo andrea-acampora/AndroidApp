0a780db96a32cdcec918eea5129cd746d8538a36
Giacomo Accursi <giacomo.accursi@gmail.com>
2021-07-10 16:53:42 +0200

    edited maps fragment

--- AndroidApp/app/src/main/java/com/example/next2me/MapsFragment.java
+++ AndroidApp/app/src/main/java/com/example/next2me/MapsFragment.java
@@ -22,6 +22,7 @@ import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,13 +41,16 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.content.Context.LOCATION_SERVICE;
@@ -54,12 +58,12 @@ import static android.content.Context.LOCATION_SERVICE;
 public class MapsFragment extends Fragment {
 
     private GoogleMap mMap;
-    private MarkerOptions userMarkerOptions;
-    private Marker userMarker;
+
     private final int LOCATION_PERMISSION_CODE = 1;
     LocationManager locationManager;
-    private LatLng currentUserPos = new LatLng(44.0575500,12.5652800);
+    private LatLng currentUserPos = new LatLng(44.0575500, 12.5652800);
     private List<String> matches = new ArrayList<>();
+    private HashMap<String, Marker> markers = new HashMap<>();
 
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
@@ -67,15 +71,112 @@ public class MapsFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
-            drawUsersOnMap();
-
-            getLocation();
+            getAllMatches();
         }
-
-
     };
 
+    /*
+    1. guardo tutti i matches accepted di una persona
+    2. per ogni id, aggiungo un listener (nel onCreate creo il marker)
+    */
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void getAllMatches(){
+
+        DatabaseReference reference = DatabaseHelper.getInstance().getDb().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("MATCHES");
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.getValue().equals("accepted")){
+                    String uid = snapshot.getKey();
+                    addListenerToMatch(uid);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    private void addListenerToMatch(String uid){
+        DatabaseReference reference = DatabaseHelper.getInstance().getDb().getReference("Users").child(uid);
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.getKey().equals("POS")){
+                    Double lat = snapshot.child("lat").getValue(Double.class);
+                    Double lng = snapshot.child("long").getValue(Double.class);
+                    Log.d("match", "lat = " + lat);
+                    Log.d("match", "long = " + lng);
+
+                    MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(lat, lng));
+
+                    Bitmap bitmap = createUserBitmap();
+                    if (bitmap != null) {
+                        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
+
+                        Marker marker = mMap.addMarker(markerOptions);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentUserPos));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentUserPos, 12));
+                        markers.put(uid, marker);
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                if(snapshot.getKey().equals("POS")){
+                    Double lat = (Double) snapshot.child("lat").getValue();
+                    Double lng = (Double) snapshot.child("long").getValue();
+                    Marker marker = markers.get(uid);
+                    if (marker != null) {
+                        marker.setPosition(new LatLng(lat, lng));
+                    }
+                }
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                String id = snapshot.getKey();
+                Marker marker = markers.get(id);
+                if (marker != null) {
+                    marker.remove();
+                }
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
 
     private Bitmap createUserBitmap() {
 
@@ -126,104 +227,5 @@ public class MapsFragment extends Fragment {
         }
         return (int) Math.ceil(getResources().getDisplayMetrics().density * value);
     }
-
-    private void getLocation() {
-        try {
-            locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
-            } else {
-                if(userMarkerOptions==null) {
-
-                    userMarkerOptions = new MarkerOptions().position(currentUserPos);
-                    Bitmap bitmap = createUserBitmap();
-                    if (bitmap != null) {
-
-                        userMarkerOptions.title("Ketan Ramani");
-                        userMarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
-                        userMarker = mMap.addMarker(userMarkerOptions);
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentUserPos));
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentUserPos, 13));
-                    }
-                }
-
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location -> {
-                    currentUserPos = new LatLng(location.getLatitude(), location.getLongitude());
-                    if(userMarkerOptions==null){
-                        Log.d("map", "entrato 2");
-
-                        /*userMarkerOptions = new MarkerOptions().position(currentUserPos);
-                        Bitmap bitmap = createUserBitmap();
-                        if (bitmap != null) {
-                            Log.d("map", "entrato 3");
-
-                            userMarkerOptions.title("Ketan Ramani");
-                            userMarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
-                            userMarker = mMap.addMarker(userMarkerOptions);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentUserPos));
-                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentUserPos, 13));
-                        }*/
-                    }else{
-                        Log.d("map", "entrato 4");
-
-                        userMarker.setPosition(currentUserPos);
-                    }
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentUserPos, 13));
-                    DatabaseHelper.getInstance().SendUserPositionToDB(currentUserPos);
-                });
-
-                userMarker.setPosition(currentUserPos);
-
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
-        }
-    }
-
-
-    private void drawUsersOnMap() {
-
-        DatabaseHelper.getInstance().getDb().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("MATCHES").equalTo("accepted").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                List<String> matches = new ArrayList<>();
-                for(DataSnapshot user : snapshot.getChildren()){
-                    if(user.getValue().toString().equals("accepted")){
-                        matches.add(user.getKey());
-                    }
-                }
-                addToMatches(matches);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void addToMatches(List<String> matches){
-        this.matches = matches; 
-    }
 }
+
